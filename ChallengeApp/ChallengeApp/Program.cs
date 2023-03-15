07a4437b23b09c294604b414f619161b901000eb
Krzysztof Matuła <gumtr@wp.pl>
2023-03-15 12:56:20 +0100

    Stworzenie pustej aplikacji

---
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -0,0 +1,35 @@
+﻿var name = "Ewa";
+var sex = "Mężczyzna";
+var age = 17;
+
+if (sex == "Kobieta")
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+
+    else if (age > 30)
+    {
+        Console.WriteLine("Kobieta powyżej 30 lat");
+    }
+
+    if (age == 33 && name == "Ewa")
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+
+}
+
+else if (sex == "Mężczyzna")
+{
+    if (age >= 18)
+    {
+        Console.WriteLine("Pelnoletni Mężczyzna");
+    }
+
+    else
+    {
+        Console.WriteLine("Niepelnoletni Mężczyzna");
+    }
+}
